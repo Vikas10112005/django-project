3abd08773f7d15e9489663dfa54b3a26c4e8760d
Vikas10112005 <vv992685@gmail.com>
2026-09-03 22:42:11 +0530

    done

---
+++ my_new_project/sos/ors/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('test/', views.test_ors),
+    path('display/', views.display),
+    path('welcome/', views.welcome),
+    path('signup/', views.user_signup),
+    path('signin/', views.user_signin),
+    path('', views.welcome),
+]
---
+++ my_new_project/sos/ors/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .service.user_service import UserService
+
+# Create your views here.
+
+def test_ors(request):
+    return HttpResponse('<h1>test ors app</h1>')
+
+def display(request):
+    return HttpResponse('<h1>this is ors display function</h1>')
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
+def user_signup(request):
+    if request.method == "POST":
+        form = {}
+        form['first_name'] = request.POST.get('firstName')
+        form['last_name'] = request.POST.get('lastName')
+        form['login_id'] = request.POST.get('loginId')
+        form['password'] = request.POST.get('password')
+        form['dob'] = request.POST.get('dob')
+        form['address'] = request.POST.get('address')
+
+        service = UserService()
+        service.add(form)
+    return render(request, 'registration.html')
+
+
+def user_signin(request):
+    print(request.POST.get('loginId'))
+    print(request.POST.get('password'))
+    return render(request,'login.html')
