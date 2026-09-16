02126e17f6bb5dd76cc65927f4e632f3bfe2fe92
Vikas10112005 <vv992685@gmail.com>
2026-09-16 14:09:14 +0530

    done

---
+++ my_new_project/my_new_project/sos/ors/views.py
@@ -0,0 +1,77 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .service.user_service import UserService
+
+
+# Create your views here.
+
+def test_ors(request):
+    return HttpResponse('<h1>test ors app</h1>')
+
+
+def display(request):
+    return HttpResponse('<h1>this is ors display function</h1>')
+
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = {}
+        form['first_name'] = request.POST.get('firstName')
+        form['last_name'] = request.POST.get('lastName')
+        form['login_id'] = request.POST.get('loginId')
+        form['password'] = request.POST.get('password')
+        form['dob'] = request.POST.get('dob')
+        form['address'] = request.POST.get('address')
+
+        service = UserService()
+        service.add(form)
+
+    return render(request, 'registration.html')
+
+
+
+def user_signin(request):
+    message = ''
+    if request.method == "POST":
+        form = {}
+        form['login_id'] = request.POST.get('loginId')
+        form['password'] = request.POST.get('password')
+
+        service = UserService()
+        records = service.authenticate(form['login_id'], form['password'])
+
+        if len(records) > 0:
+            request.session['first_name'] = records[0].get('first_name')
+            return redirect('/ors/welcome')
+        else:
+            message = 'login & password invalid'
+
+    return render(request, 'login.html', {'message': message})
+
+def logout(request):
+    request.session['first_name'] = None
+    return redirect('/ors/signin')
+
+def test_list(request):
+    list = [
+        {"id": 1, "first_name": "Rahul", "last_name": "Sharma", "email": "rahul@gmail.com", "password": "rahul123"},
+        {"id": 2, "first_name": "Priya", "last_name": "Verma", "email": "priya@gmail.com", "password": "priya123"},
+        {"id": 3, "first_name": "Amit", "last_name": "Patel", "email": "amit@gmail.com", "password": "amit123"},
+        {"id": 4, "first_name": "Neha", "last_name": "Singh", "email": "neha@gmail.com", "password": "neha123"},
+        {"id": 5, "first_name": "Rohit", "last_name": "Gupta", "email": "rohit@gmail.com", "password": "rohit123"}
+    ]
+    return render(request, "test_list.html", {"list": list})
+
+def user_list(request):
+    form = {}
+    form['page_no'] = 1
+    form['page_size'] = 5
+
+    service = UserService()
+    list = service.search(form)
+    return render(request,"user_list.html",{"list":list})
