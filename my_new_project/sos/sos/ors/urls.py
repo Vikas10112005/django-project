59950103317651dfaeb0867c280a9ec0876973b7
Vikas10112005 <vv992685@gmail.com>
2026-09-17 23:01:06 +0530

    done

--- my_new_project/sos/sos/ors/urls.py
+++ my_new_project/sos/sos/ors/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     path('display/', views.display),
     path('welcome/', views.welcome),
     path('signup/', views.user_signup),
+    path('logout/',views.logout),
     path('signin/', views.user_signin),
+    path('testlist/',views.test_list),
+    path('list/',views.user_list),
+    path('delete/<int:id>/', views.delete_user),
+    path('save/',views.user_save),
     path('', views.welcome),
 ]
--- my_new_project/sos/sos/ors/views.py
+++ my_new_project/sos/sos/ors/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .service.user_service import UserService
 
@@ -34,6 +34,7 @@ def user_signup(request):
     return render(request, 'registration.html')
 
 
+
 def user_signin(request):
     message = ''
     if request.method == "POST":
@@ -45,8 +46,66 @@ def user_signin(request):
         records = service.authenticate(form['login_id'], form['password'])
 
         if len(records) > 0:
-            return render(request, 'welcome.html', {'firstName': records[0].get('first_name')})
+            request.session['first_name'] = records[0].get('first_name')
+            return redirect('/ors/welcome')
         else:
             message = 'login & password invalid'
 
     return render(request, 'login.html', {'message': message})
+
+def logout(request):
+    request.session['first_name'] = None
+    return redirect('/ors/signin')
+
+def test_list(request):
+    list = [
+        {"id": 1, "first_name": "Rahul", "last_name": "Sharma", "email": "rahul@gmail.com", "password": "rahul123"},
+        {"id": 2, "first_name": "Priya", "last_name": "Verma", "email": "priya@gmail.com", "password": "priya123"},
+        {"id": 3, "first_name": "Amit", "last_name": "Patel", "email": "amit@gmail.com", "password": "amit123"},
+        {"id": 4, "first_name": "Neha", "last_name": "Singh", "email": "neha@gmail.com", "password": "neha123"},
+        {"id": 5, "first_name": "Rohit", "last_name": "Gupta", "email": "rohit@gmail.com", "password": "rohit123"}
+    ]
+    return render(request, "test_list.html", {"list": list})
+
+def user_list(request):
+    form = {}
+    form['page_no'] = 1
+    form['page_size'] = 5
+
+    if request.method == "POST":
+        if request.POST['operation'] == "next":
+            form['page_no'] = int(request. POST.get('pageNo'))
+            form['page_no'] += 1
+
+        if request.POST['operation'] == "previous":
+            form['page_no'] = int(request.POST.get('pageNo'))
+            form['page_no'] -= 1
+
+        if request.POST['operation'] == "search":
+            form['page_no'] = 1
+            form['first_name'] = request.POST.get('firstName')
+
+    service = UserService()
+    list = service.search(form)
+    index = (form['page_no'] - 1) * 5
+    return render(request, "user_list.html", {"list": list, 'page_no': form['page_no'],'index':index})
+
+def delete_user(request,id=0):
+    service = UserService()
+    service.delete(id)
+    return redirect('/ors/list')
+
+def user_save(request):
+    if request.method == "POST":
+        form = {}
+        form['first_name'] = request.POST.get('firstName')
+        form['last_name'] = request.POST.get('lastName')
+        form['login_id'] = request.POST.get('loginId')
+        form['password'] = request.POST.get('password')
+        form['dob'] = request.POST.get('dob')
+        form['address'] = request.POST.get('address')
+
+        service = UserService()
+        service.add(form)
+
+    return render(request, 'user.html')
