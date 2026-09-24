e6c05c002d3be6fbf97b2ae32e83f6d675109105
Vikas10112005 <vv992685@gmail.com>
2026-09-24 21:45:45 +0530

    done

--- my_new_project/my_new_project/sos/sos/ors/urls.py
+++ my_new_project/my_new_project/sos/sos/ors/urls.py
@@ -2,20 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.test_ors),
+    path('',views.welcome),
     path('welcome/', views.welcome),
     path('signup/', views.user_signup),
     path('signin/', views.user_signin),
     path('logout/', views.user_logout),
-    path('testlist/', views.test_list),
     path('list/', views.user_list),
     path('delete/<int:id>/', views.delete_user),
     path('save/', views.user_save),
-    path('edit/<int:id>/', views.edit_user),
-    path('create/', views.create_session),
-    path('access/', views.access_session),
-    path('destroy/', views.destroy_session),
-    path('set/', views.set_cookies),
-    path('get/', views.get_cookies),
-    path('',views.welcome)
+    path('save/<int:id>/', views.user_save),
 ]
